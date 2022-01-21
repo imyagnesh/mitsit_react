3af3d41f3c80e2942ba7f6301a325af19e8ed668
Yagnesh Modh <hey@yagneshmodh.com>
2022-01-21 20:05:20 +0530

    completed state and props

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./root.scss";
 
+const wait = (time: number) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
 // 1. First Later is capital
 // 2. only return single element
 // 3. apply inline style as object
@@ -27,22 +31,56 @@ type AppProp = {
 
 type Props = {};
 
-// type State = {};
+type State = {
+  a: number;
+  b: number;
+};
 
 //  manage internal memory(state)
 //  methods
 //  life cycle method
 
-class App extends Component<AppProp> {
-  //   state = {};
+// let a = 1;
+class App extends Component<AppProp, State> {
+  state = {
+    a: 1,
+    b: 2,
+  };
+
+  changeA = async () => {
+    // await wait(100);
+    console.log(this);
+
+    this.setState(
+      ({ a }) => {
+        return { a: ++a };
+      },
+      () => {
+        console.log(this.state.a);
+      }
+    );
+
+    // console.log("1");
+    // this.setState({ b: 3 });
+    // console.log("2");
+    // this.setState({ a: 4 });
+  };
 
   render() {
+    console.log("render");
+
     const { title, caption } = this.props;
+    const { a, b } = this.state;
 
     return (
       <div>
         <h1>{title}</h1>
-        <h1>{caption}</h1>
+        <h2>{caption}</h2>
+        <h3>{a}</h3>
+        <h3>{b}</h3>
+        <button type="button" onClick={this.changeA}>
+          Change Value of A
+        </button>
       </div>
     );
   }
